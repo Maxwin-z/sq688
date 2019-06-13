2cfb2325d57cc4f853c89824e6d0dacccc3d26f9
Maxwin <zwd2005@gmail.com>
2019-06-13 11:18:27 +0800

    feat: enable back & forward

--- web.js
+++ web.js
@@ -1,9 +1,33 @@
 const $ = require('./jquery-3.4.1.js')
 const webview = $('#sq688').get(0)
+
+function setNavigationStatus() {
+  console.log('<>', webview.canGoBack(), webview.canGoForward())
+  $('#btn-back').attr('disabled', !webview.canGoBack())
+  $('#btn-forward').attr('disabled', !webview.canGoForward())
+}
+
 $('#btn-pan').click(() => {
   webview.loadURL('https://pan.baidu.com')
 })
 
 $('#btn-sq688').click(() => {
   webview.loadURL('https://www.sq688.com')
+})
+
+$('#btn-back').click(() => {
+  webview.goBack()
+})
+
+$('#btn-forward').click(() => {
+  webview.goForward()
+})
+
+webview.addEventListener('dom-ready', () => {
+  console.log('ready')
+})
+
+webview.addEventListener('did-navigate', () => {
+  console.log('did-navigate')
+  setNavigationStatus()
 })
