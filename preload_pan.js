d761a9464f55c9db8bdd84e1229ac94b71de69f3
Maxwin <zwd2005@gmail.com>
2019-06-15 17:00:28 +0800

    feat: save to pan done

--- preload_pan.js
+++ preload_pan.js
@@ -1,8 +1,9 @@
+const {ipcRenderer} = require('electron')
 function waitForSelector(selector, timeout = 10000) {
   return new Promise((resolve, reject) => {
     const timer = setTimeout(() => {
-      reject('timeout', timeout)
-    })
+      reject('timeout')
+    }, timeout)
     const look = () => {
       const element = document.querySelector(selector)
       if (element) {
@@ -26,7 +27,18 @@ window.getPassword = async (password, folder) => {
     buttonForPick.click()
     buttonForSave = await waitForSelector('a[title="保存到网盘"]')
   }
-  if (buttonForSave) {
-    console.log('can save now')
+  buttonForSave.click()
+  const buttonForCreate = await waitForSelector('a[title="新建文件夹"]')
+  console.log('buttonForCreate', buttonForCreate)
+  let panFolder = document.querySelector(`span[node-path="/${folder}"]`)
+  if (!panFolder) {
+    buttonForCreate.click()
+    const newFolderInput = await waitForSelector('.shareFolderInput')
+    newFolderInput.value = folder
+    document.querySelector('.shareFolderConfirm').click()
+    panFolder = await waitForSelector(`span[node-path="/${folder}"]`)
   }
+  panFolder.click()
+  document.querySelector('.dialog-fileTreeDialog a[title="确定"]').click()
+  ipcRenderer.sendToHost('pan_saved', {})
 }
--- web.js
+++ web.js
@@ -46,48 +46,50 @@ $('#btn-songlist').click(function() {
 })
 
 $('#btn-download').click(async () => {
-  /*
+  const folder = $('#folder')
+    .val()
+    .trim()
+  if (folder.trim() === '') {
+    alert('请输入要保存到的文件夹')
+    return
+  }
+
   const pans = []
   for (let i = 0; i < songs.length; ++i) {
-    pans.push(await download(songs[i].id))
+    $('#info').html(`🔍正在获取百度盘链接${i}/${songs.length}`)
+    pans.push(await fetchPanInfo(songs[i].id))
   }
   console.log('get pans', pans)
-  */
-  save2pan()
+  $('#info').html('开始转存')
+  await save2pan(pans, `sq688_${folder}`)
+  $('#info').html('转存完成')
 })
-
 webview.addEventListener('dom-ready', () => {
   console.log('ready')
 })
-
 webview.addEventListener('did-navigate', () => {
   console.log('did-navigate')
   setNavigationStatus()
 })
-
 webview.addEventListener('new-window', (e) => {
   webview.loadURL(e.url)
 })
-
 webview.addEventListener('ipc-message', ({channel, args}) => {
   if (channel === 'songlist') {
     songs = args[0]
     const lis = songs.map((song) => $('<li>').text(song.name))
-    $('#songlist')
+    $('#songlist ol')
       .html('')
-      .append($('<ul>').append(lis))
-
+      .append(lis)
     $('#btn-download').text(`Download(${songs.length})`)
   }
 })
-
-function download(id) {
+function fetchPanInfo(id) {
   return new Promise((resolve, reject) => {
     const timeoutTimer = setTimeout(() => {
       reject('timeout')
       doClean()
     }, 100000 * 1000)
-
     let webview = document.createElement('webview')
     $(webview).css({
       position: 'fixed',
@@ -117,18 +119,18 @@ function download(id) {
     }
   })
 }
-
-function save2pan() {
-  const pan = {
-    password: 'dj3p',
-    url: 'https://pan.baidu.com/s/1dL9wduE6mx3r4h64ucVSSA'
+async function save2pan(pans, folder) {
+  for (let i = 0; i < pans.length; ++i) {
+    await _save(pans[i], folder)
+    $($('#songlist li').get(i)).append('✅')
   }
+}
+function _save(pan, folder) {
   return new Promise((resolve, reject) => {
     const timeoutTimer = setTimeout(() => {
       reject('timeout')
       doClean()
     }, 100000 * 1000)
-
     let webview = document.createElement('webview')
     $(webview).css({
       position: 'fixed',
@@ -140,17 +142,18 @@ function save2pan() {
     })
     webview.setAttribute('disablewebsecurity', true)
     webview.setAttribute('preload', './preload_pan.js')
-    webview.setAttribute('src', pan.url)
     webview.addEventListener('dom-ready', () => {
       webview.openDevTools()
-      webview.executeJavaScript(`getPassword('${pan.password}')`)
+      webview.executeJavaScript(`getPassword('${pan.password}', '${folder}')`)
     })
-    webview.addEventListener('ipc-message', ({channel, args}) => {
-      if (channel === 'pan_info') {
+    webview.addEventListener('ipc-message', ({channel}) => {
+      if (channel === 'pan_saved') {
         clearTimeout(timeoutTimer)
-        resolve(args[0])
+        resolve()
+        doClean()
       }
     })
+    webview.setAttribute('src', pan.url)
     document.body.appendChild(webview)
     function doClean() {
       document.body.removeChild(webview)
